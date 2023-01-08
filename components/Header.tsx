59d82c23085bc9ecb4f060fce6b07719ac5dacfc
sonukuldeep <sonukuldeep@outlook.com>
2023-01-08 10:35:20 +0530

    miner fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,11 @@
 import styles from '../styles/Header.module.scss'
-import { useContext, useRef } from 'react';
-import DataContext from '../context/DataContext';
 
 const Header = () => {
-    const {goTo} = useContext(DataContext)
     return (
         <header className={styles.header}>
             <div className={styles.hero}></div>
             <h1>Choose and order your dream website now!</h1>
-            <button onClick={()=>{goTo.current.scrollIntoView({behavior:"smooth"})}} className={styles.btn}>Explore</button>
+            <button className={styles.btn}>Explore</button>
         </header>
     )
 }
--- context/DataContext.tsx
+++ context/DataContext.tsx
@@ -1,5 +1,5 @@
 import React,{ createContext } from "react";
 
-const DataContext = createContext(undefined);
+const DataContext = createContext<string|undefined>(undefined);
 
 export default DataContext;
