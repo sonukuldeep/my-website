5b3d89e75bd97d6e0c7f39deefcd41901a91398c
sonukuldeep <sonukuldeep@outlook.com>
2023-01-19 21:02:01 +0530

    fixed minor bug in cart

--- components/CartItem.tsx
+++ components/CartItem.tsx
@@ -29,14 +29,15 @@ const CartItem = ({ item }: IData) => {
                 <h3 className={styles.h3}>{item.heading}</h3>
                 <h4 className={styles.h4}>{item.price}$</h4>
             </div>
-            <span onClick={() => removeItemsFromCart(setCartItems, cartItems, item.id)} className={styles.span}><button className={styles.btn}>&times;</button></span>
+            <span onClick={(event) => removeItemsFromCart(event,setCartItems, cartItems, item.id)} className={styles.span}><button className={styles.btn}>&times;</button></span>
         </div>)
     )
 }
 
 export default CartItem
 
-function removeItemsFromCart(setCartItems: React.Dispatch<React.SetStateAction<ICartItemsType[]>>, cartItems: ICartItemsType[], id: number) {
+function removeItemsFromCart(event: React.MouseEvent<HTMLSpanElement, MouseEvent>,setCartItems: React.Dispatch<React.SetStateAction<ICartItemsType[]>>, cartItems: ICartItemsType[], id: number) {
     const filteredCartItem = cartItems.filter(item => (item.id !== id))
     setCartItems(filteredCartItem)
+    event.stopPropagation()
 }
