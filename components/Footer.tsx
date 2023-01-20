98a2a269e67d608dfeb194c0b856bf7ee7f8df0a
sonukuldeep <sonukuldeep@outlook.com>
2023-01-21 01:00:59 +0530

    footer updated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,49 @@
+import React from 'react'
 import styles from '../styles/Footer.module.scss'
 
 const Footer = () => {
   return (
-    <div className={styles.container}>
-        Media icons
-        declaration
-        powered by
-    </div>
+    <footer>
+      <div className={styles.container}>
+        <div className={styles.flex}>
+          <div className={styles.columns_3}>
+            <h5>About Us</h5>
+            <ul>
+              <li><a href="#">Company Information</a></li>
+              <li><a href="#">Contact Us</a></li>
+              <li><a href="#">Careers</a></li>
+            </ul>
+          </div>
+          <div className={styles.columns_3}>
+            <h5>Support</h5>
+            <ul>
+              <li><a href="#">FAQ</a></li>
+              <li><a href="#">Help Desk</a></li>
+              <li><a href="#">Forums</a></li>
+            </ul>
+          </div>
+          <div className={styles.columns_3}>
+            <h5>Legal</h5>
+            <ul>
+              <li><a href="#">Terms of Service</a></li>
+              <li><a href="#">Privacy Policy</a></li>
+            </ul>
+          </div>
+          <div className={styles.columns_3}>
+            <h5>Social</h5>
+            <ul>
+              <li><a href="#">Facebook</a></li>
+              <li><a href="#">Twitter</a></li>
+              <li><a href="#">Instagram</a></li>
+            </ul>
+          </div>
+        </div>
+        <div className={styles.copyright}>
+            <p>Copyright &copy;{new Date().getFullYear()} EasternWebDev</p>
+        </div>
+      </div>
+    </footer>
+
   )
 }
 
