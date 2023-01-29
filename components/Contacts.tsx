41c3c266cb6400f9b9dc380ab6a292e49d0484f2
sonukuldeep <sonukuldeep@outlook.com>
2023-01-29 23:42:34 +0530

    form fields

--- components/Contacts.tsx
+++ components/Contacts.tsx
@@ -1,9 +1,41 @@
 import styles from '../styles/Contacts.module.scss'
 
 const Contacts = () => {
+  const handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    console.log('email', form.email, form.elements.email);
+    console.log('name', form.name, form.elements.name);
+  }
+
   return (
     <div className={styles.container}>
-        <h2>Contact me</h2>
+      <h2>Contact me</h2>
+      <p>Get in touch</p>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Full Name</label>
+          <input
+            id="name"
+            name="name"
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email address</label>
+          <input
+            id="email"
+            name="email"
+          />
+        </div>
+        <div>
+          <label htmlFor="msg">Message</label>
+          <textarea
+            id="msg"
+            name="msg"
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   )
 }
