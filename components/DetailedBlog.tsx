c0739189106493bcd64f4f115dae7f0fd01c286d
sonukuldeep <sonukuldeep@outlook.com>
2023-02-04 16:37:55 +0530

    bug fixes

--- components/DetailedBlog.tsx
+++ components/DetailedBlog.tsx
@@ -4,7 +4,7 @@ import styles from '../styles/DetailedBlog.module.scss'
 const DetailedBlog = () => {
     return (
         <div className={styles.container}>
-            <img className={styles.image} alt="crypto" src="https://source.unsplash.com/random/600x300" />
+            {/* <img className={styles.image} alt="crypto" src="https://source.unsplash.com/random/600x300" /> */}
             <div className={styles.features}>
                 <h1>Random heading</h1>
                 <ul>
