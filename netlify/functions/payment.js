4bd4baa2b23e6197092d7bada749de036afbf8da
sonukuldeep <sonukuldeep@outlook.com>
2023-02-08 18:38:44 +0530

    beta test 2

--- netlify/functions/payment.js
+++ netlify/functions/payment.js
@@ -5,12 +5,16 @@ const DOMAIN = "https://mywebsite0001.netlify.app"
 
 //products info -- product id is linked to pricing etc
 const product = {
-    1: process.env.ECOMMERCE_ID
+    1: process.env.ECOMMERCE_ID,
+    2: process.env.BLOG_ID,
+    3: process.env.ADVERTISE_ID,
+    4: process.env.CLONE_ID,
+    5: process.env.DESIGN_ID,
+    6: process.env.SERVICE_ID,
 };
 
 exports.handler = async (event) => {
     try {
-        console.log("this is log")
         const lineItems = [{
             price: product[1],
             quantity: 1,
