35a9ef778c988e912440a56e5fa46b16262a1e82
sonukuldeep <sonukuldeep@outlook.com>
2023-02-15 22:45:56 +0530

    demo webbsite restaurant

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/demo/restaurant',
+        destination: '/html/restaurant/restaurant.html',
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
