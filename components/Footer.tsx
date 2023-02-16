18a255a1f42b1b71ee3d8aa36d1d469486bb2106
sonukuldeep <sonukuldeep@outlook.com>
2023-02-16 20:07:22 +0530

    bug fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from '../styles/Footer.module.scss'
+import Link from 'next/link'
 
 const Footer = () => {
   return (
@@ -9,32 +10,32 @@ const Footer = () => {
           <div className={styles.columns_3}>
             <h5>About Us</h5>
             <ul>
-              <li><a href="/underconstruction">Company Information</a></li>
-              <li><a href="/underconstruction">Contact Us</a></li>
-              <li><a href="/underconstruction">Careers</a></li>
+              <li><Link href="/underconstruction">Company Information</Link></li>
+              <li><Link href="/underconstruction">Contact Us</Link></li>
+              <li><Link href="/underconstruction">Careers</Link></li>
             </ul>
           </div>
           <div className={styles.columns_3}>
             <h5>Support</h5>
             <ul>
-              <li><a href="/underconstruction">FAQ</a></li>
-              <li><a href="/underconstruction">Help Desk</a></li>
-              <li><a href="/underconstruction">Forums</a></li>
+              <li><Link href="/underconstruction">FAQ</Link></li>
+              <li><Link href="/underconstruction">Help Desk</Link></li>
+              <li><Link href="/underconstruction">Forums</Link></li>
             </ul>
           </div>
           <div className={styles.columns_3}>
             <h5>Legal</h5>
             <ul>
-              <li><a href="/underconstruction">Terms of Service</a></li>
-              <li><a href="/underconstruction">Privacy Policy</a></li>
+              <li><Link href="/underconstruction">Terms of Service</Link></li>
+              <li><Link href="/underconstruction">Privacy Policy</Link></li>
             </ul>
           </div>
           <div className={styles.columns_3}>
             <h5>Social</h5>
             <ul>
-              <li><a href="/underconstruction">Facebook</a></li>
-              <li><a href="/underconstruction">Twitter</a></li>
-              <li><a href="/underconstruction">Instagram</a></li>
+              <li><Link href="/underconstruction">Facebook</Link></li>
+              <li><Link href="/underconstruction">Twitter</Link></li>
+              <li><Link href="/underconstruction">Instagram</Link></li>
             </ul>
           </div>
         </div>
