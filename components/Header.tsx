9aecbe8a98adeb5ae52c47d15dc86091e69f074c
sonukuldeep <sonukuldeep@outlook.com>
2023-05-31 09:17:37 +0530

    type and btn fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,10 +5,13 @@ const Header = () => {
     return (
         <header className={styles.header}>
             <div className={`${styles.hero} ${full_width_style.full_width}`}></div>
+            <div>
             <h1>Order your dream website now!</h1>
             <div className={styles.hero_btns}>
-                <button className={styles.btn} onClick={()=>goToLocation("#productCategory")}>Explore</button>
-                <button className={styles.btn} onClick={openNewTab}>Order on Upwork</button>
+                <button className={styles.btn} onClick={() => goToLocation("#productCategory")}>Explore</button>
+                <button className={styles.btn} onClick={() => openNewTab(1)}>Order on Upwork</button>
+                <button className={styles.btn} onClick={() => openNewTab(2)}>Order on Fiverr</button>
+            </div>
             </div>
         </header>
     )
@@ -16,10 +19,14 @@ const Header = () => {
 
 export default Header
 
-function goToLocation(url: string){
+function goToLocation(url: string) {
     window.location.href = url
 }
-function openNewTab() {
-    const url = 'https://www.upwork.com/freelancers/~01cacedd7652ce2fa7'
+function openNewTab(key: number) {
+    const urlLink: Map<number, string> = new Map();
+    urlLink.set(1, 'https://www.upwork.com/freelancers/~01cacedd7652ce2fa7');
+    urlLink.set(2, 'https://www.fiverr.com/s/E5mbB9');
+
+    const url: string = urlLink.get(key)!
     window.open(url, '_blank', 'noreferrer');
 };
