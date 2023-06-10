56562936ce73f1287873ff05298bd9f708dd0774
sonukuldeep <sonukuldeep@outlook.com>
2023-06-10 18:21:38 +0530

    g tag added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,26 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
-// import Script from 'next/script'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
-      {/* <Script src='https://www.googletagmanager.com/gtag/js?id=G-932D5P3HNK' strategy='afterInteractive'/>
-      <Script id='google-analytics' strategy='afterInteractive'>
-        {` window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
+      {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-932D5P3HNK"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
 
-                gtag('config', 'G-932D5P3HNK');`}
-      </Script> */}
+          gtag('config', 'G-932D5P3HNK');
+        `}
+      </Script>
     </Layout>
   )
 }
