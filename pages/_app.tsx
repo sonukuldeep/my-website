787e6e0c7b958081e84095f375a798c4d09638ea
sonukuldeep <sonukuldeep@outlook.com>
2023-06-10 18:28:25 +0530

    google analytics removed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
-import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
-      {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-932D5P3HNK"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-932D5P3HNK');
-        `}
-      </Script>
     </Layout>
   )
 }
